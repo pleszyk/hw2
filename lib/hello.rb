eed9ea3936d6ab8b1cccd33644f87f1048fd3df1
Paul Leszyk <pleszyk@gmail.com>
2019-02-26 19:47:38 -0500

    Made interactive

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,5 @@
-# Default is "World"
-#Author: Paul (pleszyk@gmail.com)
-name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+puts "What's your name"
+my_name = gets.strip
+
+puts "Hello, #{my_name}!"
